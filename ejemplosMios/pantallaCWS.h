5e4bbe3738765b80ac5a3f3f988d96670d25800a
Ivan <42679240+covra@users.noreply.github.com>
2019-05-02 20:32:45 +0200

    Update pantallaCWS.h
    Añadido argumento en llamada de la funcion: tiempo en pantalla en segundos (@param tiempoPres)

--- ejemplosMios/pantallaCWS.h
+++ ejemplosMios/pantallaCWS.h
@@ -9,9 +9,9 @@
 //  v1.00  2019                                               *
 //  para XC8 v2.05                                            *
 // Incluir en el main: #include"pantallaCWS.h"                *
-// NOTAS: - Invocar la funcion: pantallaCWS(nombreProyecto)   *
-//        - Establecer frecuencia oscilador en _XTAL_FREQ     *         
-//        - Establecer el tiempo tiempoPres  en LCD           *
+// NOTAS: - Invocar la funcion:                               *
+//          pantallaCWS (nombreProyecto, num_de_segundos)     *
+//        - Establecer frecuencia oscilador en _XTAL_FREQ     *  
 //        - Incluir  "flex_lcd.h"                             *
 //          si no esta en el mismo directorio                 *
 //*************************************************************
@@ -19,15 +19,17 @@
 #include <xc.h> 
 #include "flex_lcd.h"
 
-#define tiempoPres 500  //Tiempo de permanencia en pantalla en ms
 
-void pantallaCWS (const char *stringProyecto){
+void pantallaCWS (const char *stringProyecto, unsigned char tiempoPres){
+    unsigned char i;
     Lcd_Init();
     __delay_ms(20);
     Lcd_Out(1, 0, "---CovraWorkS---");	//escribimos en la linea 2 espacio 1 la palabra mkmekatronika
-    __delay_ms(tiempoPres);
+    __delay_ms(20);
     Lcd_Out(2, 0, stringProyecto);
-    __delay_ms(tiempoPres);
+    for(i=1; i != tiempoPres; i++){
+        __delay_ms(1000);
+    }
 }
 
 #ifdef	__cplusplus
